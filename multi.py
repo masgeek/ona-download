40b2cef46352894bd3bfc0c60e7ee12af2a12ece
Sammy Barasa <barsamms@gmail.com>
2020-08-02 18:10:26 +0300

    Finalized data processig via on api

---
+++ multi.py
@@ -0,0 +1,43 @@
+import requests
+import pandas as pd
+import concurrent.futures
+import time
+import json
+import aiohttp
+import asyncio
+import OnaHelper
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+username = getenv('ONA_USERNAME')
+password = getenv('ONA_PASSWORD')
+tokenJsonFile = getenv('TOKEN_JSON')
+db_file = 'ona_form.db'
+form_list_file = 'formList.txt'
+
+rootUrl = "https://api.ona.io"
+
+onaToken = ""
+payload = ""
+helper = OnaHelper.OnaHelper(username=username, password=password, baseurl=rootUrl, db_file=db_file,
+                             form_list_file=form_list_file)
+
+data = json.loads("{}")
+
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        html = await fetch(session, f'{rootUrl}/api/v1/users/mtariku')
+        print(html)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
