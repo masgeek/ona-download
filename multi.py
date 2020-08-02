b22793a10ab2d5ca0433bdf55320fe53b9b25647
Sammy Barasa <barsamms@gmail.com>
2020-08-02 18:24:27 +0300

    Added tracker url for future user

--- multi.py
+++ multi.py
@@ -40,7 +40,7 @@ async def main():
         html = await fetch(session, f'{rootUrl}/api/v1/users/mtariku')
         print(html)
 
-
+# https://pawelmhm.github.io/asyncio/python/aiohttp/2016/04/22/asyncio-aiohttp.html
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
