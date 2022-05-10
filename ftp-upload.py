14bd04156f91bc4d99fbc5d9d6370d52b8b04665
Akilimo Machine <dev@tsobu.co.ke>
2022-05-10 11:57:13 +0300

    feat: added phploy uploads

---
+++ ftp-upload.py
@@ -0,0 +1,27 @@
+import json
+import logging
+import logging.config
+import OnaHelper
+from os import getenv, path, walk
+from dotenv import load_dotenv
+import yaml
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--user', '-u', help='Define user to download file', type=str, default='akilimo')
+parser.add_argument('--password', '-p', help='FTP password', type=str)
+
+args = parser.parse_args()
+
+load_dotenv()
+
+username = args.user
+password = args.password
+log_level = getenv('LOG_LEVEL', 'INFO')
+
+for root, dirs, files in walk('downloads/converted'):
+    for fName in files:
+        fullFileName = path.join(root, fName)
+        print(fullFileName)
